76712b7653c8e4b562aa9e4e6b1a8a86654838de
Alexander Diemand <codieplusplus@apax.net>
2019-06-23 23:44:51 +0200

    compilation succeeds in 'cli/' and 'gui/'
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- build.Linux.jenkins.sh
+++ build.Linux.jenkins.sh
@@ -8,13 +8,24 @@ nuget restore -PackagesDirectory packages packages.config
 nuget restore -PackagesDirectory packages UT/packages.config
 
 sn -k base/eLyKseeR.snk
-sn -k base/eLyKseeR-native.snk
+sn -k native/eLyKseeR-native.snk
 sn -k UT/ut.snk
 
 xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" base/eLyKseeR-base.Linux.sln
 xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" base/eLyKseeR-base.Linux.sln
 
-mono packages/NUnit.Runners.lite.2.6.4.20150512/nunit-console.exe -labels UT/bin/Debug/UT.exe
+mono packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe UT/bin/Debug/UT.exe
 
 # NUnit test results in: TestResult.xml
 
+sn -k cli/LXRbackup/LXRbackup.snk
+sn -k cli/LXRrestore/LXRrestore.snk
+
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" cli/elykseer-cli.Mono.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" cli/elykseer-cli.Mono.sln
+
+sn -k gui/LXRbackup/LXRbackup.snk
+sn -k gui/LXRrestore/LXRrestore.snk
+
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" gui/LXR_GUIs.Mono.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" gui/LXR_GUIs.Mono.sln
