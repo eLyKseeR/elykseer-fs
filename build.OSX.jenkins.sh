99acd7134092a628b52b2b6f784d10e8f3722e98
Alexander Diemand <codieplusplus@apax.net>
2019-06-26 19:01:03 +0200

    compiles on Darwin
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- build.OSX.jenkins.sh
+++ build.OSX.jenkins.sh
@@ -7,15 +7,15 @@ PLATFRM="Any CPU"
 nuget restore -PackagesDirectory packages packages.config
 nuget restore -PackagesDirectory packages UT/packages.config
 
-sn -k eLyKseeR.snk
-sn -k eLyKseeR-native.snk
+sn -k base/eLyKseeR.snk
+sn -k native/eLyKseeR-native.snk
 sn -k UT/ut.snk
 
-xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" eLyKseeR-base.OSX.sln
-xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" eLyKseeR-base.OSX.sln
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" base/eLyKseeR-base.OSX.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" base/eLyKseeR-base.OSX.sln
 
 #mono packages/NUnit.Runners.lite.2.6.4.20150512/nunit-console.exe -labels UT/bin/Debug/UT.exe
-mono packages/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe UT/bin/Debug/UT.exe
+mono packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe UT/bin/Debug/UT.exe
 
 # NUnit test results in: TestResult.xml
 
